c37d407dae70a65549ec4466bb010dac476fdd42
ypz-code <yepez.guajardo@gmail.com>
2025-10-04 21:58:59 -0600

    Update text.ts

--- client/src/utils/text.ts
+++ client/src/utils/text.ts
@@ -1,1 +1,20 @@
+export function normalize(s: string): string {
+  return s.normalize('NFC').toLowerCase().replace(/\s+/g, ' ').trim();
+}
 
+export function tokenize(s: string): string[] {
+  return normalize(s).split(/[^a-z0-9]+/).filter(Boolean);
+}
+
+export function simpleStem(token: string): string {
+  return token.replace(/(ing|ed|es|s)$/i, '');
+}
+
+export function csvEscapeCell(cell: string): string {
+  const dangerous = /^[=+\-@;]/;
+  let value = cell;
+  if (dangerous.test(value)) value = "'" + value;
+  const mustQuote = /[",\r\n]/.test(value);
+  if (mustQuote) value = '"' + value.replace(/"/g, '""') + '"';
+  return value;
+}
